6eb8b7606ea4d89b5144fafe84e9c26699e48cc3
benjadouglas <benjadouglas@gmail.com>
2023-05-18 16:58:44 -0300

    "css"
    "Algunos cambios en el css"

--- WhereToWatch/single-movie.js
+++ WhereToWatch/single-movie.js
@@ -4,16 +4,22 @@ let takeData = localStorage.getItem('movieId');
 const loadMovie = async () => {
     try {
 
+        const responseProviders= await fetch(`https://api.themoviedb.org/3/movie/${takeData}/watch/providers?api_key=dcfbb1ad76889a2f3af575f398f1ab52&language=en-US`);
+        const providers = await responseProviders.json();
+        console.log(providers)
+
         const response = await fetch(`https://api.themoviedb.org/3/movie/${takeData}?api_key=dcfbb1ad76889a2f3af575f398f1ab52&language=en-US`);
         const data = await response.json();
 
         let pelicula = '';
-        console.log(data);
         pelicula += `
             <h2 class='single-movie-title'>${data.title}</h2> 
              <div class ='single-movie-container'>
                 <img class='single-movie-image' src="https://image.tmdb.org/t/p/w500/${data.poster_path}" alt="imagen de pelicula">
-                <p class="single-movie-description">${data.overview}</p>
+                <div class='single-movie-description-container'>
+                <p class='single-movie-description'>${data.overview}</p>
+<!--                <p class='single-movie-provider'></p>-->
+                </div>
             </div> 
             <button class="single-movie-button"><a href="index.html" target="_self">Regresar</a></button>
         `;
