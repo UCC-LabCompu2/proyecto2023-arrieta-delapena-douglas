c5d2876ca4adabdffcfc44d957b99de24c34fbd1
benjadouglas <benjadouglas@gmail.com>
2023-05-22 19:28:52 -0300

    css
    Corrected some css

--- WhereToWatch/single-movie.js
+++ WhereToWatch/single-movie.js
@@ -14,18 +14,19 @@ const loadMovie = async () => {
         const response = await fetch(`https://api.themoviedb.org/3/movie/${takeData}?api_key=dcfbb1ad76889a2f3af575f398f1ab52&language=en-US`);
         const data = await response.json();
 
-        let pelicula = '';
-        pelicula += `
-            <h2 class='single-movie-title'>${data.title}</h2> 
-             <div class ='single-movie-container'>
+        let pelicula = `
+            <h2 class='single-movie-title'>${data.title}</h2>
+            <div class='single-movie-container'>
                 <img class='single-movie-image' src="https://image.tmdb.org/t/p/w500/${data.poster_path}" alt="imagen de pelicula">
-                <div class='single-movie-description-container'>
-                <p class='single-movie-description'>${data.overview}</p>
+                <div class='single-movie-details'>
+                    <div class='single-movie-description-container'>
+                        <p class='single-movie-description'>${data.overview}</p>
+                    </div>
+                    <div class='single-movie-provider-div' id='single-movie-provider-div'>
+                        <p class='single-movie-provider'>${providers}</p>
+                    </div>
                 </div>
-                <div class='single-movie-provider-div' id='single-movie-provider-div'>
-                <p class='single-movie-provider'>${providers}</p>
-                </div>
-            </div> 
+            </div>
             <button class="single-movie-button"><a href="index.html" target="_self">Regresar</a></button>
         `;
 
